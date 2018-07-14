4776b54e81736abd302b43d9d61f1731c96bcf63
Valentin Popov <info@valentineus.link>
2018-07-14 16:57:38 +0400

    Использование заданного стиля кода
    Интегрирование анализатора ESlint и исправление кода в соответствии со
    стилем MeteorJS.
    
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- server/main.js
+++ server/main.js
@@ -3,41 +3,41 @@ import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
-    /**
-     * @param {Object} options          Параметры
-     * @param {String} options.email    Электронная почта
-     * @param {String} options.password Пароль пользователя
-     * @param {String} options.token    Ключ верификации запроса
-     * @returns {Boolean}               Результат проверки
-     * @description Верифицирует пользователя по электронной почте и паролю.
-     */
-    'authorization': function (options) {
-        const logger = debug('authorization');
-        logger('Gets the package: %O', options);
+  /**
+   * @param {Object} options          Параметры
+   * @param {String} options.email    Электронная почта
+   * @param {String} options.password Пароль пользователя
+   * @param {String} options.token    Ключ верификации запроса
+   * @returns {Boolean}               Результат проверки
+   * @description Верифицирует пользователя по электронной почте и паролю.
+   */
+  authorization(options = {}) {
+    const logger = debug('authorization');
+    logger('Gets the package: %O', options);
 
-        /* Checks the token */
-        if (!('token' in options) || !(process.env.TOKEN === options.token)) {
-            return false;
-        }
-
-        /* Checks username and password */
-        if (!('email' in options) || !('password' in options)) {
-            return false;
-        }
-
-        const user = Accounts.findUserByEmail(options.email);
-        logger('Looks user: %O', user);
-
-        /* Verifies the user */
-        if (user) {
-            const check = Accounts._checkPassword(user, options.password);
-            logger('Verify password: %O', check);
-
-            if (!('error' in check) && check.userId) {
-                return true;
-            }
-        }
-
-        return false;
+    /* Checks the token */
+    if (!('token' in options) || !(process.env.TOKEN === options.token)) {
+      return false;
     }
+
+    /* Checks username and password */
+    if (!('email' in options) || !('password' in options)) {
+      return false;
+    }
+
+    const user = Accounts.findUserByEmail(options.email);
+    logger('Looks user: %O', user);
+
+    /* Verifies the user */
+    if (user) {
+      const check = Accounts._checkPassword(user, options.password);
+      logger('Verify password: %O', check);
+
+      if (!('error' in check) && check.userId) {
+        return true;
+      }
+    }
+
+    return false;
+  },
 });
--- server/main.test.js
+++ server/main.test.js
@@ -1,63 +1,65 @@
+/* eslint-env mocha */
 import '/server/main';
 import faker from 'faker';
 import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
 import { assert } from 'chai';
 
 process.env.TOKEN = faker.random.uuid();
 
 describe('Authentication Server:', () => {
-    describe('Authorization:', () => {
-        const user = {
-            password: faker.internet.password(),
-            email: faker.internet.email(),
-        };
+  describe('Authorization:', () => {
+    const user = {
+      password: faker.internet.password(),
+      email: faker.internet.email(),
+    };
 
-        before(() => {
-            Accounts.createUser(user);
-        });
-
-        it('Invalid request', (done) => {
-            Meteor.call('authorization', {
-                email: user.email,
-                password: user.password,
-                token: faker.random.uuid(),
-            }, (error, result) => {
-                assert.isFalse(result);
-                done();
-            });
-        });
-
-        it('Invalid user', (done) => {
-            Meteor.call('authorization', {
-                email: faker.internet.email(),
-                password: user.password,
-                token: process.env.TOKEN,
-            }, (error, result) => {
-                assert.isFalse(result);
-                done();
-            });
-        });
-
-        it('Invalid password', (done) => {
-            Meteor.call('authorization', {
-                email: user.email,
-                password: faker.internet.password(),
-                token: process.env.TOKEN,
-            }, (error, result) => {
-                assert.isFalse(result);
-                done();
-            });
-        });
-
-        it('Valid user', (done) => {
-            Meteor.call('authorization', {
-                email: user.email,
-                password: user.password,
-                token: process.env.TOKEN,
-            }, (error, result) => {
-                assert.isTrue(result);
-                done();
-            });
-        });
+    before(() => {
+      Accounts.createUser(user);
     });
+
+    it('Invalid request', (done) => {
+      Meteor.call('authorization', {
+        email: user.email,
+        password: user.password,
+        token: faker.random.uuid(),
+      }, (error, result) => {
+        assert.isFalse(result);
+        done();
+      });
+    });
+
+    it('Invalid user', (done) => {
+      Meteor.call('authorization', {
+        email: faker.internet.email(),
+        password: user.password,
+        token: process.env.TOKEN,
+      }, (error, result) => {
+        assert.isFalse(result);
+        done();
+      });
+    });
+
+    it('Invalid password', (done) => {
+      Meteor.call('authorization', {
+        email: user.email,
+        password: faker.internet.password(),
+        token: process.env.TOKEN,
+      }, (error, result) => {
+        assert.isFalse(result);
+        done();
+      });
+    });
+
+    it('Valid user', (done) => {
+      Meteor.call('authorization', {
+        email: user.email,
+        password: user.password,
+        token: process.env.TOKEN,
+      }, (error, result) => {
+        assert.isTrue(result);
+        done();
+      });
+    });
+  });
 });
